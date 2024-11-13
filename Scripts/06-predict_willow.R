b3a04539a7dcc2714372f0fbfc29dc25c0089ec0
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-11-12 17:33:31 -0700

    make gam table

--- Scripts/06-predict_willow.R
+++ Scripts/06-predict_willow.R
@@ -13,19 +13,52 @@ startingnuts <- readRDS("Output/Data/starting_nutrition_wide.rds")
 
 
 
-# model willow proportion availability for each height -------------------------------
+# make models for each height and extract outputs -------------------------------
 
 #write gam for low height class
 lowgam <- gam(propavail_willow ~ s(Snow) + 1, data = willow[height == "low"])
-summary(lowgam)
+sumlow <- summary(lowgam)
+
+#make s-table, and indicate model, this is for variable stuff
+sumslow <- as.data.table(round((sumlow$s.table), 4))
+sumslow[, Model := "Low"]
+sumslow[, `Dev. Explained` := round(sumlow$dev.expl, 1)]
+
 
 #gam for medium height class
 medgam <- gam(propavail_willow ~ s(Snow), data = willow[height == "medium"])
-summary(medgam)
+summed <- summary(medgam)
+
+#make s-table, and indicate model, this is for variable stuff
+sumsmed <- as.data.table(round((summed$s.table), 4))
+sumsmed[, Model := "Medium"]
+sumsmed[, `Dev. Explained` := round(summed$dev.expl, 1)]
+
 
 #gam for high height class
 highgam <- gam(propavail_willow ~ s(Snow), data = willow[height == "high"])
-summary(highgam)
+sumhigh <- summary(highgam)
+
+#make s-table, and indicate model, this is for variable stuff
+sumshigh <- as.data.table(round((sumhigh$s.table), 4))
+sumshigh[, Model := "High"]
+sumshigh[, `Dev. Explained` := round(sumhigh$dev.expl, 1)]
+
+
+
+# Merge outputs into one table -----------------------------------------------------
+
+#bind all s tables
+allsums <- rbind(sumslow, sumsmed, sumshigh)
+
+setcolorder(allsums, c("Model", "edf", "Ref.df", "F", "p-value"))
+
+#round the table to 2 decimal places
+summarytable <- allsums %>% mutate_if(is.numeric, round, digits = 2)
+
+
+
+# build predictive datasheet ----------------------------------------------
 
 #list the models
 mods <- list(lowgam, medgam, highgam)
@@ -150,5 +183,6 @@ fullplot <- ggarrange(biomassplot, CPplot, Carbplot, ncol = 1, nrow = 3)
 # save predictions --------------------------------------------------------
 
 saveRDS(modout, "Output/Data/willow_avail_prediction.rds")
+write.csv(summarytable, "Output/Tables/GAM_output_table.rds")
 ggsave("Output/Figures/Willow_avail_pred.jpeg", willow_pred, width = 5, height = 10, unit = "in")
 ggsave("Output/Figures/Total_food_avail.jpeg", fullplot, width = 5, height = 10, unit = "in")
