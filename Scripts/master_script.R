b598b9a94df98378b998f884186f1d7eed3633c0
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-02-06 15:31:16 -0700

    update master script

--- Scripts/master_script.R
+++ Scripts/master_script.R
@@ -1,5 +1,7 @@
 
-source("Scripts/01-allometric_eqn.R")
+source("Scripts/00-allometric_eqn.R")
+
+source("Scripts/01-starting_composition.R")
 
 source("Scripts/02-starting_biomass.R")
 
@@ -7,4 +9,4 @@ source("Scripts/03-camtrap_cleaning.R")
 
 source("Scripts/04-willow_availability.R")
 
-source("Scripts/05-predict_willow.R")
+source("Scripts/05-predict_willow_withnutrition.R.R")
